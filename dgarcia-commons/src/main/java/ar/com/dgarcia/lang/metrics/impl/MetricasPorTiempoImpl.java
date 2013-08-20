b4d86322109ced7a5d80a759b1925470d122cb84
dario.garcia@ikari01 <dario.garcia@ikari01>
2013-08-19 21:48:07 -0300

    Primitivas para el calculo de metricas

--- dgarcia-commons/src/main/java/ar/com/dgarcia/lang/metrics/impl/MetricasPorTiempoImpl.java
+++ dgarcia-commons/src/main/java/ar/com/dgarcia/lang/metrics/impl/MetricasPorTiempoImpl.java
@@ -12,8 +12,6 @@
  */
 package ar.com.dgarcia.lang.metrics.impl;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import ar.com.dgarcia.lang.metrics.ListenerDeMetricas;
 import ar.com.dgarcia.lang.metrics.MetricasPorTiempo;
 import ar.com.dgarcia.lang.strings.ToString;
@@ -27,33 +25,35 @@ import ar.com.dgarcia.lang.time.SystemChronometer;
  */
 public class MetricasPorTiempoImpl extends MetricasPorTiempoSupport implements MetricasPorTiempo, ListenerDeMetricas {
 
-	private AtomicLong contadorDeInputs;
+	private long contadorDeInputs;
 	public static final String contadorDeInputs_FIELD = "contadorDeInputs";
-	private AtomicLong contadorDeOutputs;
+
+	private long contadorDeOutputs;
 	public static final String contadorDeOutputs_FIELD = "contadorDeOutputs";
+
 	private SystemChronometer cronometro;
 	public static final String cronometro_FIELD = "cronometro";
 
 	/**
 	 * @see net.gaia.vortex.core.api.metricas.MetricasPorTiempo#getCantidadDeInputs()
 	 */
-	
+
 	public long getCantidadDeInputs() {
-		return contadorDeInputs.get();
+		return contadorDeInputs;
 	}
 
 	/**
 	 * @see net.gaia.vortex.core.api.metricas.MetricasPorTiempo#getCantidadDeOutputs()
 	 */
-	
+
 	public long getCantidadDeOutputs() {
-		return contadorDeOutputs.get();
+		return contadorDeOutputs;
 	}
 
 	/**
 	 * @see net.gaia.vortex.core.api.metricas.MetricasPorTiempo#getDuracionDeMedicionEnMilis()
 	 */
-	
+
 	public long getDuracionDeMedicionEnMilis() {
 		return cronometro.getElapsedMillis();
 	}
@@ -67,23 +67,23 @@ public class MetricasPorTiempoImpl extends MetricasPorTiempoSupport implements M
 	/**
 	 * Registra en esta métrica que se realizó una recepción de mensaje
 	 */
-	
+
 	public void registrarInput() {
-		this.contadorDeInputs.incrementAndGet();
+		this.contadorDeInputs++;
 	}
 
 	/**
 	 * Registra en esta métrica que se realizó un ruteo de mensaje
 	 */
-	
+
 	public void registrarOutput() {
-		this.contadorDeOutputs.incrementAndGet();
+		this.contadorDeOutputs++;
 	}
 
 	/**
 	 * @see net.gaia.vortex.core.api.metricas.MetricasPorTiempo#getMomentoDeInicioDeLaMedicionEnMilis()
 	 */
-	
+
 	public long getMomentoDeInicioDeLaMedicionEnMilis() {
 		return cronometro.getStartMillis();
 	}
@@ -93,30 +93,31 @@ public class MetricasPorTiempoImpl extends MetricasPorTiempoSupport implements M
 	 */
 	public void resetear() {
 		this.cronometro = SystemChronometer.create();
-		this.contadorDeInputs = new AtomicLong();
-		this.contadorDeOutputs = new AtomicLong();
+		this.contadorDeInputs = 0;
+		this.contadorDeOutputs = 0;
 	}
 
 	/**
 	 * @see ar.com.dgarcia.lang.metrics.ListenerDeMetricas#registrarInput(long)
 	 */
-	
+
 	public void registrarInput(final long cantidadIngresada) {
-		this.contadorDeInputs.addAndGet(cantidadIngresada);
+		this.contadorDeInputs += cantidadIngresada;
 	}
 
 	/**
 	 * @see ar.com.dgarcia.lang.metrics.ListenerDeMetricas#registrarOutput(long)
 	 */
-	
+
 	public void registrarOutput(final long cantidadEgresada) {
-		this.contadorDeOutputs.addAndGet(cantidadEgresada);
+		this.contadorDeOutputs += cantidadEgresada;
 	}
 
 	/**
 	 * @see java.lang.Object#toString()
 	 */
-	
+
+	@Override
 	public String toString() {
 		return ToString.de(this).con(contadorDeInputs_FIELD, contadorDeInputs)
 				.con(contadorDeOutputs_FIELD, contadorDeOutputs).con(cronometro_FIELD, cronometro).toString();
